ebd5973f0a9388965ecee28c1ef0f5703df7c838
Angelo_Cossio <angelo.cossio@icloud.com>
2021-01-30 21:53:28 -0300

    Fix excepción.

--- src/main/java/com/devops/dxc/devops/model/Dxc.java
+++ src/main/java/com/devops/dxc/devops/model/Dxc.java
@@ -3,6 +3,7 @@ package com.devops.dxc.devops.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 public class Dxc implements Serializable{
 

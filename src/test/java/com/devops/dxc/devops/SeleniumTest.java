e1e3ed935a210710f932e02d480a2fe832a978be
Cristian <cristian.cubillos@editrade.cl>
2021-02-02 20:53:17 -0300

    test selenium

--- src/test/java/com/devops/dxc/devops/SeleniumTest.java
+++ src/test/java/com/devops/dxc/devops/SeleniumTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -80,58 +81,61 @@ public class SeleniumTest {
     }
 
     @Test
+    @Order(1)
     public void cuandoSueldoEs1000000Ahorro10000000RetiroEs1000000MetodoCalcula10() throws Exception {
         sendSolicitud(3400000.0, 10000000.0, 387604.0, 9612396.0, 612396.0);
     }
 
     @Test
+    @Order(2)
     public void cuandoSueldoEs2000000Ahorro10000000RetiroEs8254690MetodoCalcula10() throws Exception {
         sendSolicitud(2000000.0, 10000000.0, 825469.0, 9174531.0, 174531.0);
     }
 
     @Test
+    @Order(3)
     public void cuandoSueldoEs3400000Ahorro10000000RetiroEs387604MetodoCalcula10() throws Exception {
         sendSolicitud(3400000.0, 10000000.0, 387604.0, 9612396.0, 612396.0);
     }
 
     @Test
+    @Order(4)
     public void cuandoSueldoEs4700000Ahorro10000000RetiroEs19643MetodoCalcula10() throws Exception {
         sendSolicitud(4700000.0, 10000000.0, 19643.0, 9980357.0, 980357.0);
     }
 
     @Test
+    @Order(5)
     public void cuandoSueldoEs6100000Ahorro20000000RetiroEs508958MetodoCalcula10() throws Exception {
         sendSolicitud(6100000.0, 20000000.0, 508958.0, 19491042, 1491042.0);
     }
 
-    /*@Test
-    @Order(2)
-    public void testInputLetras() {
-        testErrorInput("aa", "bb");
-    }*/
-
- /* @Test
-    @Order(2)
-    public void testInputNegativos() {
-        testErrorInput("-1", "-2");
-    }*/
- /*@Test
-    @Order(3)
-    public void reiniciarForm() {
-        formPage();
-        Assertions.assertEquals(true, true);
-    }*/
     @Test
+    @Order(6)
     public void cuandoSueldoEs5100000Ahorro20000000RetiroEs705948MetodoCalcula10() throws Exception {
         sendSolicitud(5100000.0, 20000000.0, 705948.0, 19294052.0, 1294052.0);
     }
 
     @Test
+    @Order(7)
     public void cuandoSueldoEs2700000Ahorro10000000RetiroEs635914MetodoCalcula10() throws Exception {
         sendSolicitud(2700000.0, 10000000.0, 635914.0, 9364086.0, 364086.0);
     }
 
+    @Test
+    @Order(8)
+    public void testInputLetras() {
+        testErrorInput("aa", "bb");
+    }
+
+    @Test
+    @Order(9)
+    public void testInputNegativos() {
+        testErrorInput("-1", "-2");
+    }
+
     private void testErrorInput(String strSaldo, String strSueldo) {
+        formPage();
         WebElement inputSaldo = driver.findElement(By.id("saldo"));
         Assertions.assertEquals(true, inputSaldo.isDisplayed());
 
@@ -149,12 +153,13 @@ public class SeleniumTest {
         inputSueldo.click();
         inputSueldo.sendKeys(strSueldo);
 
-        inputBoton.click();
-
-        sleep(2000);
-        String textAlert = driver.switchTo().alert().getText();
-        System.out.println("TextAlert: " + textAlert.trim());
-        Assertions.assertEquals(true, textAlert.trim().contains("Hubo un problema"));
+        try {
+            inputBoton.click();
+        } catch (org.openqa.selenium.UnhandledAlertException e) {
+            String textAlert = driver.switchTo().alert().getText();
+            System.out.println("TextAlert: " + textAlert.trim());
+            Assertions.assertEquals(true, textAlert.trim().contains("Hubo un problema"));
+        }
     }
 
     private void sendSolicitud(double sueldo, double saldo, double retiroEsperado, double saldoRestanteEsperado, double impuestoEsperado) {
